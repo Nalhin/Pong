6416eb373fe5a40b4c27426da0ee52da62f1737f
Nalhin <kris.wow@wp.pl>
2019-05-30 11:50:11 +0200

    Added peddle class and onkeydown event to window

--- script.js
+++ script.js
@@ -17,13 +17,13 @@ class Board {
 }
 
 class Player {
-    constructor(positionx, positiony, radious, color,dx,dy) {
+    constructor(positionx, positiony, radious, color, dx, dy) {
         this.x = positionx;
         this.y = positiony;
         this.r = radious;
         this.color = color;
-        this.dx=dx;
-        this.dy=dy;
+        this.dx = dx;
+        this.dy = dy;
     }
     drawPlayer() {
         ctx.beginPath();
@@ -31,17 +31,28 @@ class Player {
         ctx.fillStyle = this.color;
         ctx.fill();
     }
-    changePosition(){
-        this.x+=this.dx;
-        this.y+=this.dy;
+    changePosition() {
+        this.x += this.dx;
+        this.y += this.dy;
     }
-    checkCollision(){
-        this.x+this.r>board.width ? console.log("right") :
-        this.x -this.r <0 ? console.log("left") :
-        this.y -this.r < 0 ? console.log("top") :
-        this.y + this.r > board.height ? console.log ("bottom") :null;
+    checkCollision() {
+        this.x + this.r > board.width ? console.log("right") :
+            this.x - this.r < 0 ? console.log("left") :
+                this.y - this.r < 0 ? console.log("top") :
+                    this.y + this.r > board.height ? console.log("bottom") : null;
 
     }
+}
+
+class Paddle {
+    constructor(positionx, positiony, width, height, color) {
+        this.x = positionx;
+        this.y = positiony;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
 }
 
 function gameLoop() {
@@ -53,11 +64,26 @@ function gameLoop() {
 }
 
 function newGame() {
-    player = new Player(20, 400, 10, 'black',2,2);
+    player = new Player(20, 400, 10, 'black', 2, 2);
     board = new Board(canvas.width, canvas.height, 'grey');
-     
+
 }
 
 newGame();
 window.requestAnimationFrame(gameLoop);
-// center x, center y, radious, start angle, end angle
+
+window.addEventListener('keydown', (event) => {
+    switch (event.keyCode) {
+
+        case 83:  //"s" key
+          //  peddle.changePosition();
+            break;
+
+        case 65:  //"a" key
+            peddle.changePosition();
+           // break;
+        default:
+            break;
+    }
+
+});
