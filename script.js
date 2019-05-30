0dc65d011b48f3aefb33005f3bc5b5a02f61ca82
Nalhin <kris.wow@wp.pl>
2019-05-30 11:37:15 +0200

    Added player collision detection and player position change methods

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("c");
 const ctx = canvas.getContext("2d");
 
-let board, player;
+let player, board;
 
 class Board {
     constructor(width, height, backgroundColor) {
@@ -17,29 +17,47 @@ class Board {
 }
 
 class Player {
-    constructor(positionx, positiony, radious, color) {
+    constructor(positionx, positiony, radious, color,dx,dy) {
         this.x = positionx;
         this.y = positiony;
         this.r = radious;
         this.color = color;
+        this.dx=dx;
+        this.dy=dy;
     }
     drawPlayer() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+        ctx.fillStyle = this.color;
         ctx.fill();
+    }
+    changePosition(){
+        this.x+=this.dx;
+        this.y+=this.dy;
+    }
+    checkCollision(){
+        this.x+this.r>board.width ? console.log("right") :
+        this.x -this.r <0 ? console.log("left") :
+        this.y -this.r < 0 ? console.log("top") :
+        this.y + this.r > board.height ? console.log ("bottom") :null;
+
     }
 }
 
 function gameLoop() {
     board.drawBoard();
     player.drawPlayer();
-
+    player.changePosition();
+    player.checkCollision();
+    window.requestAnimationFrame(gameLoop);
 }
 
 function newGame() {
+    player = new Player(20, 400, 10, 'black',2,2);
     board = new Board(canvas.width, canvas.height, 'grey');
-    player = new Player(50, 50, 10, 'black');
+     
 }
-window.requestAnimationFrame(gameLoop);
 
+newGame();
+window.requestAnimationFrame(gameLoop);
 // center x, center y, radious, start angle, end angle
