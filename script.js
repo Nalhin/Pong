023b8aac185bceb45a97bc6b433a9f32c3d91206
Nalhin <krol.wow@wp.pl>
2019-06-01 14:58:40 +0200

    done

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-const canvas = document.getElementById("c");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('c');
+const ctx = canvas.getContext('2d');
 
-let ball, paddle, board;
+let ball, paddleOne, paddleTwo, board;
 
 class Board {
     constructor(width, height, backgroundColor) {
         this.width = width;
         this.height = height;
         this.backgroundColor = backgroundColor;
+        this.score = 0;
     }
     drawBoard() {
         ctx.clearRect(0, 0, this.width, this.height) //clears board
         ctx.fillStyle = this.backgroundColor;
         ctx.fillRect(0, 0, this.width, this.height);
+        ctx.fillStyle = 'white';
+        ctx.fillRect((canvas.width/2)-5,0,10,this.height);
+     
+    }
+    drawScore(paddle) {
+        ctx.font = '30px Arial';
+        ctx.fillStyle = 'white';
+        ctx.fillText(paddle.score, paddle.x, this.height - 6);
     }
 }
 
@@ -36,36 +45,43 @@ class Ball {
         this.y += this.dy;
     }
     checkWallCollision() {
-        this.x + this.r > board.width ? this.dx=-this.dx :  //right
-            this.x - this.r < 0 ? this.dx=-this.dx :     //left
-                this.y - this.r < 0 ? this.dy=-this.dy :      //top
-                    this.y + this.r > board.height ? console.log("bottom") : null;  //bottom
-
+        this.y - this.r < 0 ? this.dy = -this.dy :  //top
+            this.y + this.r > board.height ? this.dy = -this.dy :  //bottom
+                this.x + this.r > board.width ? newGame() :  //right
+                    this.x - this.r < 0 ? newGame() : null;   //left
     }
-    checkPaddleCollision() {
-        (this.x > paddle.x && this.x + this.r <paddle.x + paddle.width) &&
-            (this.y > paddle.y && this.y < paddle.y + paddle.height) ?
-            this.dy=-this.dy : null;
 
-    }
 }
 
 class Paddle {
-    constructor(width, height, color) {
+    constructor(width, height, x, y, color) {
         this.width = width;
         this.height = height;
-        this.x = (canvas.width - this.width) / 2;
-        this.y = canvas.height - this.height;
+        this.x = x;
+        this.y = y;
         this.color = color;
+        this.score = 0;
     }
     drawPaddle() {
         ctx.beginPath();
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-    changePosition(dx) {
-        this.x + dx + this.width < canvas.width && this.x + dx > 0 ?
-            this.x += dx : null;
+    changePosition(dy) {
+        this.y + dy + this.height < canvas.height && this.y + dy > 0 ?
+            this.y += dy : null;
+    }
+    checkPaddleCollision() {    //ok
+        (ball.x > this.x- ball.r  && ball.x < this.x + this.width + ball.r) &&
+            (ball.y > this.y && ball.y < this.y + this.height) ?
+            this.paddleHit() : null;
+    }
+    paddleHit() {
+        ball.dx = -ball.dx;
+        this.score++;
+    }
+    setPosition() {
+        this.y = ball.y - this.height / 2;
     }
 }
 
@@ -74,16 +90,21 @@ function gameLoop() {
     ball.drawBall();
     ball.changePosition();
     ball.checkWallCollision();
-    paddle.drawPaddle();
-    ball.checkPaddleCollision();
+    paddleOne.drawPaddle();
+    paddleTwo.drawPaddle();
+    paddleOne.checkPaddleCollision();
+    paddleTwo.checkPaddleCollision();
+    paddleTwo.setPosition();
+    board.drawScore(paddleOne);
+    board.drawScore(paddleTwo);
     window.requestAnimationFrame(gameLoop);
 }
 
 function newGame() {
-    ball = new Ball(20, 400, 10, 'black', 2, 2);
-    board = new Board(canvas.width, canvas.height, 'grey');
-    paddle = new Paddle(140, 20, 'black')
-
+    ball = new Ball(canvas.width / 2, canvas.height / 2, 10, 'yellow', 4, 4); //x,y,radious,color,dx,dy
+    board = new Board(canvas.width, canvas.height, 'grey');  //width,height,color
+    paddleOne = new Paddle(20, 120, 0, canvas.height / 2, 'black') // width,height,x,y,color
+    paddleTwo = new Paddle(20, 120, canvas.width - 20, canvas.height / 2, 'black')
 }
 
 newGame();
@@ -91,17 +112,13 @@ window.requestAnimationFrame(gameLoop);
 
 window.addEventListener('keydown', (event) => {
     switch (event.keyCode) {
-
         case 68:  //'d' key
-        console.log('xd')
-            paddle.changePosition(8);
+            console.log('xd')
+            paddleOne.changePosition(8);
             break;
-
         case 65:  //'a' key
-            paddle.changePosition(-8);
-        // break;
+            paddleOne.changePosition(-8);
         default:
             break;
     }
-
 });
